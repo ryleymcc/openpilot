fc0c15eabed1977075b4d1196172410486cc1ce1
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2022-01-04 23:34:34 -0600

    disable extra hud warnings
    Signed-off-by: Jafar Al-Gharaibeh <to.jafar@gmail.com>

--- selfdrive/car/mazda/carcontroller.py
+++ selfdrive/car/mazda/carcontroller.py
@@ -60,9 +60,10 @@ class CarController:
     # send HUD alerts
     if self.frame % 50 == 0:
       ldw = CC.hudControl.visualAlert == VisualAlert.ldw
-      steer_required = CC.hudControl.visualAlert == VisualAlert.steerRequired
+      # steer_required = CC.hudControl.visualAlert == VisualAlert.steerRequired
       # TODO: find a way to silence audible warnings so we can add more hud alerts
-      steer_required = steer_required and CS.lkas_allowed_speed
+      #steer_required = steer_required and CS.lkas_allowed_speed
+      steer_required = CS.out.steerFaultTemporary
       can_sends.append(mazdacan.create_alert_command(self.packer, CS.cam_laneinfo, ldw, steer_required))
 
     # send steering command
