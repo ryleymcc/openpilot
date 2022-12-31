224e98cc217cc748c8e07ffd8bb1c83ca6de86ca
moretore <ryleymccarroll@gmail.com>
2022-12-31 12:02:36 -0600

    update interface.py for RI

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -18,13 +18,22 @@ class CarInterface(CarInterfaceBase):
     ret.carName = "mazda"
 
     ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.mazda)]
-    ret.radarOffCan = True
+    ret.radarOffCan = False
 
     ret.dashcamOnly = False # candidate not in (CAR.CX5_2022, CAR.CX9_2021)
 
     #ret.enableTorqueInterceptor = 0x24A in fingerprint[0]
     #if ret.enableTorqueInterceptor:
     #  print("Recieving torque interceptor signal.")
+    ret.openpilotLongitudinalControl = True
+    ret.longitudinalTuning.kpBP = [0., 5., 30.]
+    ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
+    ret.longitudinalTuning.kiBP = [0., 5., 20., 30.]
+    ret.longitudinalTuning.kiV = [0.36, 0.23, 0.17, 0.1]
+    ret.longitudinalTuning.deadzoneBP = [0.0, 30.0]
+    ret.longitudinalTuning.deadzoneV = [0.0, 0.03]
+    ret.longitudinalActuatorDelayLowerBound = 0.3
+    ret.longitudinalActuatorDelayUpperBound = 1.5
 
     ret.steerActuatorDelay = 0.1
     ret.steerLimitTimer = 0.8
